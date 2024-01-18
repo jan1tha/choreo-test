b1309c61b84a3edc01a15e670985b7fe9aafbe87
janitha <jnsenevirathna@google.com>
2024-01-18 11:47:28 +0530

    Added demo spring boot project

---
+++ choreo-spring-boot/src/main/java/com/wso2/choreospringboot/ChoreoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.wso2.choreospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoreoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoreoSpringBootApplication.class, args);
+	}
+
+}
