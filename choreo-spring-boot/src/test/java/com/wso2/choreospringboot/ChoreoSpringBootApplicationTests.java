0c8bf594af2004e9a303d9baaf2f2558adc94a00
janitha <jnsenevirathna@google.com>
2024-01-18 12:24:24 +0530

    Removed tests for testing

--- choreo-spring-boot/src/test/java/com/wso2/choreospringboot/ChoreoSpringBootApplicationTests.java
+++ choreo-spring-boot/src/test/java/com/wso2/choreospringboot/ChoreoSpringBootApplicationTests.java
@@ -1,13 +1,13 @@
-package com.wso2.choreospringboot;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ChoreoSpringBootApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.wso2.choreospringboot;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ChoreoSpringBootApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
